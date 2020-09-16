b980cfdf3f9fa16e7aa5a580b605ce976219dee1
Sasso <ricciuti.alessio@hotmail.com>
2020-09-16 19:25:54 +0200

    Added node js modules

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const errorPage = 'HTML/404.html';
+const listenPort = 8080;
 
 http.createServer(function (req, res) {
-    fs.readFile('HTML/index.html', function(err, data) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
-}).listen(8080);
+}).listen(listenPort);
 
 console.log('Server has started!');
