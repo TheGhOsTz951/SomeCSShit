0ba5c84b0f10449ce1c3deee4eeecefbac80b740
Sasso <ricciuti.alessio@hotmail.com>
2020-09-19 13:32:46 +0200

    Small Changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const errorPage = '/public/html/404.html'
-const listenPort = 8080
+const mainPath = '/public/html/index.html'
+const port = 8090
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/html/index.html')
+    res.sendFile(__dirname + mainPath)
 })
 
 app.get('*', (req, res) => {
@@ -14,7 +15,7 @@ app.get('*', (req, res) => {
     res.status(404).sendFile(__dirname + errorPage)
 })
 
-app.listen(listenPort, () => {
-    console.log(`Pagina web in ascolto su \x1b[91mhttp://localhost:${listenPort}`, '\x1b[0m')
+app.listen(port, () => {
+    console.log(`Pagina web in ascolto su \x1b[91mhttp://localhost:${port}`, '\x1b[0m')
     console.log('\x1b[94mServer has started!', '\x1b[0m')
 })
